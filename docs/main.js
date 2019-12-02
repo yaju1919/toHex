be24cedd742b4876410ac19a5ae553b85df7b3f4
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2019-12-02 14:17:28 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,7 @@
     const yaju1919 = yaju1919_library;
     const unit = 4; // 単位文字数
     const shape0 = (n,str) => ("0".repeat(n) + str).slice(-n).toUpperCase(); // strを左から0詰めでn文字に整形
-    const encode = (str, key = 0) => insert_space(r(str.replace(/(.|\n)/g, c => shape0(unit,overflow(Number(c.charCodeAt(0)) + key).toString(16).slice(-unit)))));
+    const encode = (str, key = 0) => insert_space(r(str.trim().replace(/(.|\n)/g, c => shape0(unit,overflow(Number(c.charCodeAt(0)) + key).toString(16).slice(-unit)))));
     const decode = (str, key = 0) => r(str.replace(/[^0-9A-Fa-f]/g,"")).replace(new RegExp("[0-9A-Fa-f]{" + unit + "}", "g"), n => String.fromCharCode(overflow(parseInt(n, 16) - key)));
 
     const reverse = str => str.split('').reverse().join(''); // 文字列の反転
@@ -39,19 +39,44 @@
         if(!navigator.clipboard.readText) return;
         appendBtn("貼り付け", () => navigator.clipboard.readText().then(text=>textarea().val(text)));
     })();
-    appendBtn("暗号化", () => {
-        const v = input().trim();
-        if(isHex(v)) return message("これ以上暗号化できません");
-        result(encode(v,input_key()));
-    });
-    appendBtn("復元", () => {
-        const v = input().trim();
-        if(!isHex(v)) return message("既に復元されています");
-        result(decode(v,input_key()));
-    });
+    appendBtn("暗号化", () => result(encode(input(),input_key())));
+    appendBtn("復元", () => result(decode(input(),input_key())));
     h.append("<br>");
-    const message_elm = $("<div>").appendTo(h);
-    const message = str => message_elm.text(str + '(' + yaju1919.getTime() + ')');
+    yaju1919.appendCheckButton(h,{
+        title: "オプション機能",
+        change: f => f ? h_opt.show() : h_opt.hide()
+    });
+    //------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------
+    const h_opt = $("<div>").appendTo(h).hide();
+    const space_flag = yaju1919.appendCheckButton(h_opt,{
+        title: "半角スペースを挿入する",
+        value: true
+    });
+    const input_key = yaju1919.appendInputNumber(h_opt,{
+        title: "暗号化キー",
+        width: "5em",
+        max: BIG_NUMBER,
+        min: -BIG_NUMBER,
+        value: 0,
+        int: true
+    });
+    const reverse_flag = yaju1919.appendCheckButton(h_opt,{
+        title: "反転処理",
+        change: f => f ? h_rev.show() : h_rev.hide()
+    });
+    const h_rev = $("<span>").appendTo(h_opt).hide();
+    const input_reverse = yaju1919.appendInputNumber(h_rev,{
+        title: "反転キー",
+        width: "5em",
+        max: 1000,
+        min: 2,
+        value: 2,
+        int: true
+    });
+    h_opt.children().each(e=>$(e).append("<br>"));
+    //------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------
     const input = yaju1919.appendInputText(h,{
         placeholder: "暗号化 or 復元したい文章をここに入力してください。",
         width: "50%",
@@ -66,45 +91,18 @@
         return false;
     });
     h.append("<br>");
-    h.append("オプション機能<br>");
-    const space_flag = yaju1919.appendCheckButton(h,{
-        title: "半角スペースを挿入する",
-        value: true
-    });
-    h.append("<br>");
-    const input_key = yaju1919.appendInputNumber(h,{
-        title: "暗号化キー",
-        width: "5em",
-        max: BIG_NUMBER,
-        min: -BIG_NUMBER,
-        value: 0,
-        int: true
-    });
-    h.append("<br>");
-    const reverse_flag = yaju1919.appendCheckButton(h,{
-        title: "反転処理",
-        change: f => f ? h_rev.show() : h_rev.hide()
-    });
-    const h_rev = $("<span>").appendTo(h).hide();
-    const input_reverse = yaju1919.appendInputNumber(h_rev,{
-        title: "反転キー",
-        width: "5em",
-        max: 1000,
-        min: 2,
-        value: 2,
-        int: true
-    });
-    h.append("<br>");
     h.append("<br>");
     h.append("<br>結果　");
     appendBtn("コピー", () => {
         yaju1919.copy(result_elm.text().trim());
     });
-    const result_elm = $("<div>").appendTo(h);
+    const result_elm = $("<div>").appendTo(h).css({
+        backgroundColor: "pink"
+    });
     const result = text => {
         result_elm.empty();
         text.split('\n').map(e=>{
-            result_elm.append(e);
+            result_elm.append(e+'\n');
             result_elm.append("<br>");
         });
     }
